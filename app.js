f2b19490c9071022da7f576d157777a82cf44a4b
1988db <elozrana@gmail.com>
2022-06-12 10:30:06 +0200

    feat: input checkbox type add

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    console.log('DOMContentLoaded');
-});
+
+
+const narodziny = new Date(1983,6,25)
+const teraz = new Date();
+
+let wiek = teraz - narodziny;
+console.log(wiek/1000/60/60/24/365)
+  
+
+  
+ });
+
